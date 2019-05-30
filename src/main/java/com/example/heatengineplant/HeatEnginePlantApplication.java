d9662108cb04ec3946427e37b068a51f5061648d
S16201302 <1204805820@qq.com>
2019-05-30 20:19:33 +0800

    初始化项目

---
+++ src/main/java/com/example/heatengineplant/HeatEnginePlantApplication.java
@@ -0,0 +1,13 @@
+package com.example.heatengineplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatEnginePlantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeatEnginePlantApplication.class, args);
+    }
+
+}
